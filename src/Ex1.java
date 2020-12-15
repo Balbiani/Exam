7f39ad4d3eb8dfc25f1d45f01787dff9f18c4b9a
Balbiani <balbiani.antoine@gmail.com>
2020-12-15 17:32:07 +0100

    monCommit

---
+++ src/Ex1.java
@@ -0,0 +1,19 @@
+public class Item {
+    private String name;
+
+    public Item(String getName){
+        name= getName;
+    }
+    private Long price;
+
+    public Item(Long getPrice){
+        price=getPrice;
+    }
+    public Main(){
+        Item item = new Item("corn flakes", 500);
+        printf():String.format("%d.%02d")
+        System.out.println(item);
+        Item chewingGum = new Item("chewing gum",403);
+        printf():String.format("%.5s%:")
+        System.out.println(chewingGum);     }
+}
